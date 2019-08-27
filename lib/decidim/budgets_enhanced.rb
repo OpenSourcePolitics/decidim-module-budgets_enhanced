f91ddd679b07c276dec64392c15cc7dbbfdb0d74
Armand <fardeauarmand@gmail.com>
2019-08-27 16:08:11 +0200

    Remove useless component file

--- lib/decidim/budgets_enhanced.rb
+++ lib/decidim/budgets_enhanced.rb
@@ -3,7 +3,6 @@
 require "decidim/budgets_enhanced/admin"
 require "decidim/budgets_enhanced/engine"
 require "decidim/budgets_enhanced/admin_engine"
-#require "decidim/budgets_enhanced/component"
 
 module Decidim
   # This namespace holds the logic of the `BudgetsEnhanced` component. This component
