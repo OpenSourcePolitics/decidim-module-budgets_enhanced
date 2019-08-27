60eea33a1f7df0b02ad705b2492df153c3560d80
Armand <fardeauarmand@gmail.com>
2019-08-27 15:45:30 +0200

    Fix missing helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,9 @@ ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 Decidim::Dev.dummy_app_path = File.expand_path(File.join("spec", "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
+
+Dir["#{__dir__}/rspec_support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include WaitForAjax
+end
