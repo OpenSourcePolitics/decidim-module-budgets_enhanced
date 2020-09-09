c65ebca742ba1ba2240be088405fdb4ddb3a039a
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-09-09 18:13:43 +0300

    Add missing method for 0.22

--- app/models/decidim/budgets/project.rb
+++ app/models/decidim/budgets/project.rb
@@ -33,6 +33,10 @@ module Decidim
         datetime: :created_at
       )
 
+      def self.ordered_ids(ids)
+        order(Arel.sql("position(id::text in '#{ids.join(",")}')"))
+      end
+
       def self.log_presenter_class_for(_log)
         Decidim::Budgets::AdminLog::ProjectPresenter
       end
